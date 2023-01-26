908c1436459e4439c4c6bd5c1d7ffc02178a67da
Drlinx <dragonlinkx@gmail.com>
2023-01-26 13:48:14 -0700

    hello functions written

--- hello1.c
+++ hello1.c
@@ -1,3 +1,23 @@
+/**
+ * @file hello1.c
+ * 
+ * @author Kassidy Maberry
+ * 
+ * @date 2023/01/26
+ * 
+ * Assignment: hello world
+ * 
+ * @brief A simple hello world program.
+ * 
+ * @detail None
+ * 
+ * @bugs none
+ * 
+ * @todo none
+ */
+
+#include <stdio.h>
+
 int main (void){
     printf("Hello World!\n");
     return 1;
---
+++ hello2.c
@@ -0,0 +1,24 @@
+/**
+ * @file hello2.c
+ * 
+ * @author Kassidy Maberry
+ * 
+ * @date 2023/01/26
+ * 
+ * Assignment: hello world
+ * 
+ * @brief A simple hello name program.
+ * 
+ * @detail None
+ * 
+ * @bugs none
+ * 
+ * @todo none
+ */
+
+#include <stdio.h>
+
+int main (int argv, char *argc){
+    printf("Hello %s\n", argc);
+    return 1;
+}
